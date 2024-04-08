464f5f7030f201c45a4594965149ed8e46415177
Kapil Chaudhary <kc685838@gmail.com>
2024-04-09 01:49:06 +0530

    Added: default layout

---
+++ nuxt-components/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div class="text-center text-2xl font-bold max-w-screen-xl mx-auto py-4">This is the Header Section.</div>
+        <div class="max-w-screen-xl mx-auto">
+            <slot />
+        </div>
+        <div class="text-center max-w-screen-xl mx-auto py-4">This is the Footer Section.</div>
+    </div>
+</template>
---
+++ nuxt-components/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        This is the Index page.
+    </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: "Home Page",
+    meta: [
+        { 
+            name: "description", 
+            content: "This is the Index page description."
+        }
+    ]
+});
+</script>
